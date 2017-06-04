4f3f3dd4bb31e224a765884b8c575338bc78f478
Tiziano Pessa <tiziano.pessa@dada.eu>
2017-06-04 11:56:12 +0200

    Lettura payroll e caricamento del select box dei mesi

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,48 @@
-var AppView = Backbone.View.extend({
-    el: '#content',
+var monthName = _.template("<% switch (month) {" +
+    "case 1: print('Gennaio'); break;" +
+    "case 2: print('Febbraio'); break;" +
+    "case 3: print('Marzo'); break;" +
+    "case 4: print('Aprile'); break;" +
+    "case 5: print('Maggio'); break;" +
+    "case 6: print('Giugno'); break;" +
+    "case 7: print('Luglio'); break;" +
+    "case 8: print('Agosto'); break;" +
+    "case 9: print('Settembre'); break;" +
+    "case 10: print('Ottobre'); break;" +
+    "case 11: print('Novembre'); break;" +
+    "case 12: print('Dicembre'); break;" +
+"} %>");
+
+var LeaveCollection = Backbone.Collection.extend({
+    url: '/api/leaves'
+});
+
+var PayrollCollection = Backbone.Collection.extend({
+    url: '/api/payrolls'
+});
+
+var MainView = Backbone.View.extend({
+    el: '#main',
+
     initialize: function() {
-        this.render();
+        this.listenTo(this.collection, 'reset', this.fillMonthSelectOptions);
+
+        this.collection.fetch({reset: true});
     },
-    render: function() {
-        this.$el.html("Ciao ciao ciao");
+
+    fillMonthSelectOptions: function() {
+        var monthSelector = this.$el.find('#month');
+        monthSelector.empty();
+        this.collection.each(function(payroll) {
+           monthSelector.prepend($('<option/>')
+               .val(payroll.get('id'))
+               .text(monthName({month: payroll.get('month')}) + ' ' + payroll.get('year')));
+        });
     }
 });
 
 $(document).ready(function () {
-    var view = new AppView();
+    var main = new MainView({
+        collection: new PayrollCollection()
+    });
 });
